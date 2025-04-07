210e555dd977a69c91568f1a836d26e22c4e003f
shivam dhir <shivamdhir90@gmail.com>
2025-04-06 23:53:15 -0400

    add save and fetch games functionality

--- src/main/java/com/shivam/controller/GameController.java
+++ src/main/java/com/shivam/controller/GameController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shivam.model.Game;
 import com.shivam.repository.GameRepository;
 
+//@RestController -> API, only returns data, not view
 @RestController
 @RequestMapping("api")
 public class GameController {
@@ -18,9 +21,18 @@ public class GameController {
     @Autowired  
     GameRepository gamerepo;
 
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/")
     public String redirect(){
         return "hello";
+    }
+
+    // To use @RequestBody, you annotate a method parameter in your controller with it. 
+    // When a request is made to the endpoint, Spring Boot will read the request body and attempt to convert it to the specified 
+    // type of the annotated parameter. If the conversion is successful, the method will be invoked with the populated object. 
+    // If the conversion fails, a HttpMessageNotReadableException will be thrown
+    @PostMapping(value = "/addGame")
+    public Game add(@RequestBody Game game){
+        return gamerepo.save(game); 
     }
 
     @GetMapping(value="/games")
