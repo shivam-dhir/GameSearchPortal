7ba5e89bd78058e51c3868f2c2291f1d00a9d523
shivam130502 <shivamdhir90@gmail.com>
2025-04-16 22:52:50 -0700

    creating authentication layer

--- src/main/java/com/shivam/controller/PortalSecurity.java
+++ src/main/java/com/shivam/controller/PortalSecurity.java
@@ -1,21 +1,39 @@
 package com.shivam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class PortalSecurity {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    // Verifying username and password from database
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return daoAuthenticationProvider;
+    }
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
                                 .username("user")
-                                .password("password")
+                                .password(  "password")
                                 .roles("USER")
                                 .build();
         return new InMemoryUserDetailsManager(user);
